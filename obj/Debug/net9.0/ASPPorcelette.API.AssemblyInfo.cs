810acc5365b31e1aa1c15d95d572dd2476d4a54e
Looka <napoa322@gmail.com>
2025-10-21 17:56:58 +0200

    modif selon le front evenement

--- obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c8bd7cb5531e826033acc24cf0e5a935cdb99b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6430484371cda588a0b5061c8ab03010c2c56620")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
