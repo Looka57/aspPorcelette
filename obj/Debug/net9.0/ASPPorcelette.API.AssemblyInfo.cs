727074739663979ae8245b4da5f427ca3043203c
Looka <napoa322@gmail.com>
2025-10-22 17:42:58 +0200

    changement du senseiId en user dans la table Actualité... plus creation V2 Bdd

--- obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810acc5365b31e1aa1c15d95d572dd2476d4a54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4aa96266e59772d74ee0a66ae9e94e9a18a6098")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
