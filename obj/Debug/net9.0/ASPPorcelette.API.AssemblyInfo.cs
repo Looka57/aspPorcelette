ac838e44e358e6907a182d804e0a289b1483e387
Looka <napoa322@gmail.com>
2025-10-29 18:35:23 +0100

    mise en place squemette page profil avec le lien

--- obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASPPorcelette.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7966d0423da393ad810f0e420ae3b3888b7936db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753da0101b4ec420585cd8d36eb2d0f264115573")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
