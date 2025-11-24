75e3211bcc6fa74950fabc98cc829e0b17c0b585
Looka <napoa322@gmail.com>
2025-11-24 15:13:40 +0100

    Mise à jour des fichiers binaires et des informations d'assemblage pour la version 1.0.1

--- obj/Debug/net10.0/ASPPorcelette.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/ASPPorcelette.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2fb7f2a5e1b4e8da9281086a29f40f41cca277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9642752c39ec3518177bd314347ad3ac2856e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
