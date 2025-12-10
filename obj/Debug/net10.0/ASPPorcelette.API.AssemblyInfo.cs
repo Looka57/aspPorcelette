713640df4fc1dd176519ec9ab195977b60ae66a9
Looka <napoa322@gmail.com>
2025-12-10 09:05:09 +0100

    Mise à jour de la version d'information de l'assemblage et du cache des entrées d'assemblage

--- obj/Debug/net10.0/ASPPorcelette.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/ASPPorcelette.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3dfac05013d4f7f2e4ea1165feb7c813eadbad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3259dc67d2e3554b88fb7ce94ea53f430a44f057")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPPorcelette.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
